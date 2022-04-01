3a551ce5aa28c0d3eaa821778e08019b78266e0c
zusheng <imzusheng@163.com>
2022-04-01 15:54:57 +0800

    update: 提取主题色

--- src/views/PlaylistView.vue
+++ src/views/PlaylistView.vue
@@ -173,36 +173,9 @@ export default {
   watch: {
     '$store.getters.playlistPicUrl': {
       handler (imgSrc) {
-        const canvas = document.createElement('canvas')
-        const ctx = canvas.getContext('2d')
-        canvas.width = 200
-        canvas.height = 200
-        const img = new Image()
-        img.setAttribute('crossOrigin', '')
-        img.src = imgSrc
-        img.onload = () => {
-          ctx.drawImage(img, 0, 0, 200, 200)
-          const data = ctx.getImageData(0, 0, 200, 200).data
-          const color = getMainColor(data)
-          const rgb = color.split(',')
-          let r = parseInt(rgb[0])
-          let g = parseInt(rgb[1])
-          let b = parseInt(rgb[2])
-          if (r > 200 || g > 200 || b > 200) {
-            r -= 80
-            g -= 80
-            b -= 80
-          } else if (r > 150 || g > 150 || b > 150) {
-            r -= 40
-            g -= 40
-            b -= 40
-          } else if (r < 50 || g < 50 || b < 50) {
-            r += 80
-            g += 80
-            b += 80
-          }
-          this.$store.state.curPlaylistColor = `${r}, ${g}, ${b}, 1`
-        }
+        getMainColor(imgSrc).then(color => {
+          this.$store.state.curPlaylistColor = color
+        })
       }
     }
   },
