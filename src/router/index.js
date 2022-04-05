1b1eda6f85f957e6f6a3536903612fb7f62f0dfa
zusheng <imzusheng@163.com>
2022-04-05 12:50:56 +0800

    update: router history

--- src/router/index.js
+++ src/router/index.js
@@ -33,7 +33,7 @@ const routes = [
   }]
 
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes,
   scrollBehavior (to, from, savedPosition) {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-netease-cloud-music/' : '/'
 })
